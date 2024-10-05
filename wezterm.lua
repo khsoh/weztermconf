7d267d4c2d5fadc010f4c366502b88a01cb6f3ad
K H Soh <hju37823@outlook.com>
2024-10-05 13:20:57 +0800

    Initial commit of wezterm config

---
+++ wezterm.lua
@@ -0,0 +1,75 @@
+local wezterm = require("wezterm")
+
+local config = wezterm.config_builder()
+
+config.color_scheme = "Catppuccin Mocha"
+
+config.font = wezterm.font("FiraMono Nerd Font", { weight = "Medium" })
+
+config.font_size = 16
+
+config.window_decorations = "RESIZE"
+
+-- tmux
+-- config.leader = { key = "a", mods = "CTRL", timeout_milliseconds = 2000 }
+local act = wezterm.action
+config.leader = { key = "a", mods = "CTRL" }
+config.keys = {
+	{ key = "h", mods = "CTRL", action = act.ActivatePaneDirection("Left") },
+	{ key = "k", mods = "CTRL", action = act.ActivatePaneDirection("Down") },
+	{ key = "j", mods = "CTRL", action = act.ActivatePaneDirection("Up") },
+	{ key = "l", mods = "CTRL", action = act.ActivatePaneDirection("Right") },
+	{
+		key = "h",
+		mods = "LEADER",
+		action = act.SplitHorizontal({
+			domain = "CurrentPaneDomain",
+		}),
+	},
+	{
+		key = "v",
+		mods = "LEADER",
+		action = act.SplitVertical({
+			domain = "CurrentPaneDomain",
+		}),
+	},
+}
+
+-- tab bar
+config.hide_tab_bar_if_only_one_tab = false
+config.tab_bar_at_bottom = true
+config.use_fancy_tab_bar = false
+config.tab_and_split_indices_are_zero_based = true
+
+local weztmux = wezterm.plugin.require("https://github.com/khsoh/wez-tmux")
+-- !!! For debugging changes in local repo
+-- local weztmux = wezterm.plugin.require("file://localhost" .. os.getenv("HOME") .. "/github/wez-tmux")
+weztmux.apply_to_config(config, {})
+
+-- The following must be applied AFTER weztmux.apply_to_config
+config.leader.timeout_milliseconds = 2000
+
+-- tmux status
+wezterm.on("update-right-status", function(window, _)
+	local SOLID_LEFT_ARROW = ""
+	local ARROW_FOREGROUND = { Foreground = { Color = "#c6a0f6" } }
+	local prefix = ""
+
+	if window:leader_is_active() then
+		prefix = " " .. utf8.char(0x1f30a) -- ocean wave
+		SOLID_LEFT_ARROW = utf8.char(0xe0b2)
+	end
+
+	if window:active_tab():tab_id() ~= 0 then
+		ARROW_FOREGROUND = { Foreground = { Color = "#1e2030" } }
+	end -- arrow color based on if tab is first pane
+
+	window:set_left_status(wezterm.format({
+		{ Background = { Color = "#b7bdf8" } },
+		{ Text = prefix },
+		ARROW_FOREGROUND,
+		{ Text = SOLID_LEFT_ARROW },
+	}))
+end)
+
+return config
