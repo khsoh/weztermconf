34859f710a2ef276298eb1c7cffd08d228005bc3
K H Soh <hju37823@outlook.com>
2024-10-19 11:36:54 +0800

    Moved tab bar to top

--- wezterm.lua
+++ wezterm.lua
@@ -38,7 +38,7 @@ config.window_close_confirmation = "NeverPrompt"
 
 -- tab bar
 config.hide_tab_bar_if_only_one_tab = false
-config.tab_bar_at_bottom = true
+config.tab_bar_at_bottom = false
 config.use_fancy_tab_bar = false
 config.tab_and_split_indices_are_zero_based = true
 
